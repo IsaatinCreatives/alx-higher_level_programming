b5e44d093d4c449167bbd66d10599f3f4775b3d3
Akintunde Taiwo <aitaiwo01@gmail.com>
2023-01-05 20:17:59 -0800

    A program that imports the variable a from the file variable_load_5.py and prints its value

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,5 @@ if __name__ == "__main__":
     else:
         print("{:d} arguments:".format(len(argv) - 1))
     for arg in argv[1:]:
-        print("{:d}: {:s}".format(i, arg))i += 1
+        print("{:d}: {:s}".format(i, arg))
+        i += 1
