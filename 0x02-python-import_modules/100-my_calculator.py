e0ed85f2201181ce4374ff708188d3d8f2cf3dcb
Akintunde Taiwo <aitaiwo01@gmail.com>
2023-01-10 07:40:40 -0800

    A program that imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,10 +2,10 @@
 def main(argv):
     argc = len(argv)
     ops = {
-            '+': calculator_1.add,
-            '-': calculator_1.sub,
-            '*': calculator_1.mul,
-            '/': calculator_1.div,
+        '+': calculator_1.add,
+        '-': calculator_1.sub,
+        '*': calculator_1.mul,
+        '/': calculator_1.div,
     }
     if argc != 4:
         print('Usage: {:s} <a> <operator> <b>'.format(argv[0]))
@@ -18,8 +18,9 @@ def main(argv):
         exit(1)
     res = ops[op](a, b)
     print('{:d} {:s} {:d} = {:d}'.format(a, op, b, res))
-    
-    if __name__ == '__main__':
-        from sys import argv, exit
-        import calculator_1
-        main(argv)
+
+
+if __name__ == '__main__':
+    from sys import argv, exit
+    import calculator_1
+    main(argv)
