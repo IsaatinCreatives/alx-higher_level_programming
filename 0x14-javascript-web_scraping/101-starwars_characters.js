d12d7e856d52ba35e9e84eb3d0b919a66e76dbe7
Akintunde Taiwo <aitaiwo01@gmail.com>
2023-05-24 18:09:28 -0700

    update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
-// prints all characters of starwars movie
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    JSON.parse(body).characters.forEach((item) => {
-      request(item, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
-        } else throw error;
-      });
-    });
-  } else throw error;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
